203a8ccd1b9aad18699f9aef96607543def2f8a5
MykolasVitkus <47564825+MykolasVitkus@users.noreply.github.com>
2019-04-02 17:41:07 +0300

    added bootstrap styles

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap';
+
+require('../css/app.css');
+
+
 const Preview = () => (<div>Hi, I am a simple div rendered in react</div>);
 
 ReactDOM.render(<Preview />, document.getElementById('root'));
