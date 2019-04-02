a29dc4189dbe63df357765434b7fe7f49c316717
MykolasVitkus <47564825+MykolasVitkus@users.noreply.github.com>
2019-04-02 17:31:22 +0300

    jquery+popper install

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'bootstrap';
 const Preview = () => (<div>Hi, I am a simple div rendered in react</div>);
 
 ReactDOM.render(<Preview />, document.getElementById('root'));
